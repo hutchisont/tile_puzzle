787cdeccf5856819a55c6ae36af1efc5222b2d5d
Tyler Hutchison <tylerhutchison96@gmail.com>
2025-07-16 22:41:10 -0500

    Make scripts do release builds

--- run/vars.bash
+++ run/vars.bash
@@ -4,4 +4,12 @@ export root_dir=$(git rev-parse --show-toplevel)
 export out_dir="${root_dir}/build"
 export src_dir="${root_dir}/src"
 export exe_name="tile_puzzle"
-export default_build_flags=(-vet -strict-style -warnings-as-errors "-out:\"${out_dir}/${exe_name}\"")
+export default_build_flags=(
+	-vet
+	-strict-style
+	-warnings-as-errors
+	-o:size
+	-microarch:native
+	"-out:\"${out_dir}/${exe_name}\""
+	-show-timings
+)
